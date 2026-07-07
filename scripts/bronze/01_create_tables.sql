63de73eb69bb8250500b3a410bb85c2d8c121e7b
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-07 06:16:46 +0300

    Edit data types in bronze.customers

--- scripts/bronze/01_create_tables.sql
+++ scripts/bronze/01_create_tables.sql
@@ -25,13 +25,13 @@ GO
 
 CREATE TABLE bronze.customers
 (
-    customer_id VARCHAR(15) NULL,
-    customer_name VARCHAR(50) NULL,
-    customer_type VARCHAR(50) NULL,
-    credit_terms_days VARCHAR(50) NULL,
-    primary_freight_type VARCHAR(50) NULL,
-    account_status VARCHAR(50) NULL,
-    contract_start_date VARCHAR(50) NULL,
-    annual_revenue_potential VARCHAR(50) NULL
+    customer_id NVARCHAR(15) NULL,
+    customer_name NVARCHAR(50) NULL,
+    customer_type NVARCHAR(50) NULL,
+    credit_terms_days INT NULL,
+    primary_freight_type NVARCHAR(50) NULL,
+    account_status NVARCHAR(50) NULL,
+    contract_start_date DATE NULL,
+    annual_revenue_potential INT NULL
 );
 GO
