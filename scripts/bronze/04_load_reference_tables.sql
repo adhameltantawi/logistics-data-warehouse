75533dab24cc8b5667c8168eb59d7ef659086b55
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-08 00:21:36 +0300

    start loading ref in bronze layer

---
+++ scripts/bronze/04_load_reference_tables.sql
@@ -0,0 +1,74 @@
+
+
+TRUNCATE TABLE bronze.drivers
+BULK INSERT bronze.drivers
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\drivers.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.customers
+BULK INSERT bronze.customers
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\customers.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.facilities
+BULK INSERT bronze.facilities
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\facilities.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.routes
+BULK INSERT bronze.routes
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\routes.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.trailers
+BULK INSERT bronze.trailers
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\trailers.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.trucks
+BULK INSERT bronze.trucks
+FROM 'D:\data\data projects\logistics-data-warehouse\datasets\reference\trucks.csv'
+WITH
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '0x0A',
+    TABLOCK
+);
+
+-- SELECT * FROM bronze.drivers
+-- SELECT * FROM bronze.customers
+-- SELECT * FROM bronze.facilities
+-- SELECT * FROM bronze.routes
+-- SELECT * FROM bronze.trailers
+-- SELECT * FROM bronze.trucks
