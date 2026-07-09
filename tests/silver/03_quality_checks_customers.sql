9112548445997379bb11385a5d0188efee9b7012
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-09 19:47:31 +0300

    Add SQL quality checks for customers data in bronze layer

---
+++ tests/silver/03_quality_checks_customers.sql
@@ -0,0 +1,42 @@
+SELECT
+    customer_id,
+    customer_name,
+    customer_type,
+    credit_terms_days,
+    primary_freight_type,
+    account_status,
+    contract_start_date,
+    annual_revenue_potential
+FROM bronze.customers;
+
+
+SELECT customer_id
+FROM bronze.customers
+WHERE customer_id IS NULL;
+
+
+SELECT 
+    customer_id,
+    COUNT(*)
+FROM bronze.customers
+GROUP BY customer_id
+HAVING COUNT(*) > 1;
+
+
+SELECT 
+    customer_name
+FROM bronze.customers
+WHERE customer_name != TRIM(customer_name);
+
+
+SELECT
+    customer_name,
+    COUNT(DISTINCT customer_id) AS customer_count
+FROM bronze.customers
+GROUP BY customer_name
+HAVING COUNT(DISTINCT customer_id) > 1;
+
+
+SELECT *
+FROM bronze.customers
+WHERE customer_name = 'ABC Corp';
