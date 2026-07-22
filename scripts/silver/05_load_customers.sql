e51aef950508cc854a73bdaed6eea883e5f51ff3
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-23 01:36:53 +0300

    add column names

--- scripts/silver/05_load_customers.sql
+++ scripts/silver/05_load_customers.sql
@@ -11,5 +11,12 @@ INSERT INTO silver.customers
 )
 
 SELECT
-    *
+    customer_id,
+    customer_name,
+    customer_type,
+    credit_terms_days,
+    primary_freight_type,
+    account_status,
+    contract_start_date,
+    annual_revenue_potential
 FROM bronze.customers;
