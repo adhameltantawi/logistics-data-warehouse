a9d5836d0927ec3a61315d4fcf3295507674e23a
Adham Eltantawi <adhameltantawi@gmail.com>
2026-07-23 04:08:58 +0300

    Check facility_name

--- tests/silver/05_quality_checks_facilities.sql
+++ tests/silver/05_quality_checks_facilities.sql
@@ -3,3 +3,10 @@ FROM bronze.facilities
 WHERE facility_id IS NULL
    OR facility_id = ''
    OR LEN(facility_id) != 8;
+
+
+SELECT facility_name
+FROM bronze.facilities
+WHERE facility_name IS NULL
+   OR facility_name = ''
+   OR facility_name != TRIM(facility_name);
